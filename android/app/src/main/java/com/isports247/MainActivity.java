3010180ae2ed413c9e4824c9f61bc82e627b1a2b
asim sagir <asim.sagir@gmail.com>
2021-06-07 15:27:48 +0530

    modification src folder and files

---
+++ android/app/src/main/java/com/isports247/MainActivity.java
@@ -0,0 +1,21 @@
+package com.isports247;
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "isports247";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+  }
+}
